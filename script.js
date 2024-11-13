4a809313f8129f29c82c82d3f0aea695405e586c
MFatkhurRozaq <fathurrozaq1588@gmail.com>
2024-11-13 09:21:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const navBar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () =>{
+    const positionWindow = window.scrollY > 0;
+    navBar.classList.toggle('scrolling-active', positionWindow);
+});
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+  });
